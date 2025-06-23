a5ff81fa2ce6713f99e3400f75cccde43d0c83e6
cullback <cullback@fastmail.com>
2025-06-22 23:04:05 -0400

    add Game trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Game {
+    fn new() -> u64;
+    fn update(&self, state: u64) -> u64;
 }
